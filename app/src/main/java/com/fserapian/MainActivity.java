139c381af44a6d8e7f0c764ac3f191f31cba316e
Fadi Serapian <fadi.serapian@gmail.com>
2019-11-14 11:25:21 -0500

    Minor Change

--- app/src/main/java/com/fserapian/MainActivity.java
+++ app/src/main/java/com/fserapian/MainActivity.java
@@ -25,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
-    private boolean gameOver = false;
     private int roundsCount = 0;
+    private boolean gameOver = false;
     private boolean macsTurn = true;
 
     public void fadeIn(View view) {
